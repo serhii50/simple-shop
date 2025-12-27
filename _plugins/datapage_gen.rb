d18177de3516e6169170b15f98bef87660217adc
serhii50 <serhii.v.kirichenko@gmail.com>
2025-12-27 11:44:10 -0500

    Fix slugify method call in datapage_gen.rb
    Fixed error by adding 'Jekyll::' before Utils in filename generation.

--- _plugins/datapage_gen.rb
+++ _plugins/datapage_gen.rb
@@ -7,7 +7,9 @@ module Jekyll
       
       # Формируем имя файла
       filename = name_expr ? data[name_expr].to_s : name
-      @name = Utils.slugify(filename) + ".html"
+      
+      # ВОТ ТУТ БЫЛА ОШИБКА. Добавил "Jekyll::" перед Utils
+      @name = Jekyll::Utils.slugify(filename) + ".html"
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), template + '.html')
@@ -33,7 +35,6 @@ module Jekyll
         if site.data.key?(data_file)
           records = site.data[data_file]
           records.each do |record|
-            # Создаем страницу для каждой строки CSV
             site.pages << DataPage.new(site, site.source, dir, record, nil, name_prop, template)
           end
         end
